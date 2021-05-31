4d9463ce97f1955ec6f1c34ae873bfcefdeaa810
Lukas Pühringer <lukas.puehringer@stud.fh-campuswien.ac.at>
2021-05-31 21:37:46 +0200

    Test Case - Password Numbers 2

--- src/test/java/PasswordTest.java
+++ src/test/java/PasswordTest.java
@@ -59,8 +59,16 @@ public class PasswordTest {
     @DisplayName("Check Password for Numbers")
     public void testPasswordNumbers(){
         Password p = new Password("123445123323");
+        assertTrue(p.checkPasswordLetters(), "Password needs Numbers!");
+    }
+
+    @Test
+    @DisplayName("Check Password for no Numbers")
+    public void testPasswordNoNumbers(){
+        Password p = new Password("NoNumbers");
         assertFalse(p.checkPasswordLetters(), "Password needs Numbers!");
     }
+
 
 
 
